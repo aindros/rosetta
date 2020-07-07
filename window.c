f8be0be3c1c3381c10910f2f22ce95ef4ad01d83
aindros <aindros@hotmail.com>
2020-07-07 16:22:59 +0200

    Workaround to get a star in the title when editing a file

--- window.c
+++ window.c
@@ -25,12 +25,16 @@
 #include <string.h>
 
 gboolean	 rosetta_window_on_key_press(GtkWidget *widget, GdkEventKey *event, gpointer user_data);
+gboolean	 rosetta_window_on_key_release(GtkWidget *widget, GdkEventKey *event, gpointer user_data);
+
+char *basename(char *);
 
 GtkWidget
 *rosetta_window_new(Rosetta *rosetta, const char *app_title)
 {
   char *filename = rosetta->argc >= 1 ? rosetta->argv[1] : NULL;
   rosetta->filename = filename;
+  rosetta->basename = basename(rosetta->filename);
 
   /* Editor */
   rosetta->editor = rosetta_editor_new(filename);
@@ -45,8 +49,8 @@ GtkWidget
   /* Menubar */
   rosetta_menu_bar_new(rosetta);
 
-  if (rosetta->filename != NULL) {
-    gtk_window_set_title(GTK_WINDOW(rosetta->window), rosetta->filename);
+  if (rosetta->basename != NULL) {
+    gtk_window_set_title(GTK_WINDOW(rosetta->window), rosetta->basename);
   } else {
     gtk_window_set_title(GTK_WINDOW(rosetta->window), app_title);
   }
@@ -63,6 +67,10 @@ GtkWidget
   g_signal_connect (G_OBJECT(rosetta->window),
                     "key_press_event",
                     G_CALLBACK (rosetta_window_on_key_press),
+                    rosetta);
+    g_signal_connect (G_OBJECT(rosetta->window),
+                    "key_release_event",
+                    G_CALLBACK (rosetta_window_on_key_release),
                     rosetta);
 
   gtk_widget_show_all(rosetta->window);
@@ -82,6 +90,7 @@ rosetta_window_on_key_press(GtkWidget *widget, GdkEventKey *event, gpointer user
     if (event->state & GDK_CONTROL_MASK) {
       if (SSM(SCI_GETMODIFY, 0, 0) == TRUE) {
         if (rosetta->filename != NULL) {
+          printf("Saved\n");
           rosetta_editor_save_file(sci, rosetta->filename);
         } else {
           rosetta_save_file_dialog(rosetta);
@@ -91,8 +100,58 @@ rosetta_window_on_key_press(GtkWidget *widget, GdkEventKey *event, gpointer user
     break;
 
   default:
-    return FALSE; 
+    break;
+  }
+
+  /* After CTRL+S is pressed and file saved, check modifications: */
+  if (SSM(SCI_GETMODIFY, 0, 0) == TRUE) {
+    char *title = (char *) malloc(sizeof(char*) * (strlen(rosetta->basename) + 1));
+    strcpy(title, rosetta->basename);
+    strcat(title, "*");
+    gtk_window_set_title(GTK_WINDOW(rosetta->window), title);
+  } else {
+    char *title = (char *) malloc(sizeof(char*) * (strlen(rosetta->basename) + 1));
+    strcpy(title, rosetta->basename);
+    /*strcat(title, "*");*/
+    gtk_window_set_title(GTK_WINDOW(rosetta->window), title);
   }
 
   return FALSE; 
+}
+
+/*
+ * It's a workaround to get title working. If user presses a key and file got modified, rosetta will change the title
+ * adding a trailing star (*). I guess we don't do this stuff like this, but I can't find any resource to check after
+ * a pressed key if the file got changed.
+ */
+gboolean
+rosetta_window_on_key_release(GtkWidget *widget, GdkEventKey *event, gpointer user_data)
+{
+  Rosetta *rosetta = (Rosetta *) user_data;
+  ScintillaObject *sci = SCINTILLA(rosetta->editor);
+
+  if (SSM(SCI_GETMODIFY, 0, 0) == TRUE) {
+    char *title = (char *) malloc(sizeof(char*) * (strlen(rosetta->basename) + 1));
+    strcpy(title, rosetta->basename);
+    strcat(title, "*");
+    gtk_window_set_title(GTK_WINDOW(rosetta->window), title);
+  }
+
+  return FALSE;
+}
+
+char
+*basename(char *str)
+{
+  char *pfile;
+  pfile = str + strlen(str);
+
+  for (; pfile > str; pfile--) {
+    if ((*pfile == '\\') || (*pfile == '/')) {
+      pfile++;
+      break;
+    }
+  }
+
+  return pfile;
 }
